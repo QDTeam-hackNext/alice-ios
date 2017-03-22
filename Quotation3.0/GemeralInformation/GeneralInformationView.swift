50f49af71de702ee8d3084184b653ae2b1135ef7
Dariusz Luksza <dariusz@luksza.org>
2017-03-22 19:38:08 +0100

    Fix compilation issues

--- Quotation3.0/GemeralInformation/GeneralInformationView.swift
+++ Quotation3.0/GemeralInformation/GeneralInformationView.swift
@@ -12,19 +12,18 @@ class GeneralIfomationView: UIViewControllerWithViewModel<GeneralInformationView
   @IBOutlet weak var recordButton: UIButton!
 
   override func viewDidLoad() {
-    self.model = GeneralInformationViewModel(recorder: Recorder())
     Speech().speek("I hope that it will sound like Siri and won't be necessary to use Siri explicitly")
   }
 
   @IBAction func recordButtonTouchDown(_ sender: Any) {
-    self.model?.startRecord()
+    self.model.startRecord()
   }
 
   @IBAction func recordButtonTouchUpInside(_ sender: Any) {
-    self.model?.stopRecord()
+    self.model.stopRecord()
   }
 
   @IBAction func recordButtonTouchUpOutside(_ sender: Any) {
-    self.model?.stopRecord()
+    self.model.stopRecord()
   }
 }
