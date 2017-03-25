d3c0280873d0f9c11f40e9e70a010c8503b55ec6
Dariusz Luksza <dariusz@luksza.org>
2017-03-25 09:18:21 +0100

    Add overlay to quotation view

--- Quotation3.0/UserStory/UserStoryView.swift
+++ Quotation3.0/UserStory/UserStoryView.swift
@@ -26,7 +26,18 @@ class UserStoryView: UIViewController, WithViewModel {
   @IBOutlet weak var aliceTextLabel: UILabel!
   @IBOutlet weak var applyButton: UIButton!
 
+  @IBOutlet weak var container2: UIView!
+  @IBOutlet weak var alice1Label: UILabel!
+  @IBOutlet weak var user1Label: UILabel!
+  @IBOutlet weak var alice2Label: UILabel!
+  @IBOutlet weak var user3Label: UILabel!
+  @IBOutlet weak var alice3Label: UILabel!
+  @IBOutlet weak var waveView: WaveformView!
+  @IBOutlet weak var recordingBacground: UILabel!
+  @IBOutlet weak var recordButton: UIButton!
+
   override func viewDidLoad() {
+    self.container2.isHidden = true
     self.view.backgroundColor = UIColor.background
 
     self.container.layer.cornerRadius = 8
@@ -58,6 +69,59 @@ class UserStoryView: UIViewController, WithViewModel {
     self.applyButton.clipsToBounds = true
     self.applyButton.titleLabel?.font = UIFont.buttonFontFont()
     self.applyButton.setTitleColor(UIColor.white, for: .normal)
+
+    self.container2.backgroundColor = UIColor.clear
+
+    let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.prominent)
+    let blurEffectView = UIVisualEffectView(effect: blurEffect)
+    blurEffectView.alpha = 0.9
+    blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    blurEffectView.frame = CGRect(x: 0, y: 0,
+                                  width: self.container2.frame.width,
+                                  height: self.container2.frame.height - self.recordingBacground.frame.height)
+    self.container2.addSubview(blurEffectView)
+    self.container2.addSubview(self.alice1Label)
+    self.container2.addSubview(self.alice2Label)
+    self.container2.addSubview(self.alice3Label)
+    self.container2.addSubview(self.user1Label)
+    self.container2.addSubview(self.user3Label)
+    self.container2.addSubview(self.waveView)
+    self.container2.addSubview(self.dividerLabel)
+
+    self.styleAliceLable(label: self.alice1Label)
+    self.styleAliceLable(label: self.alice2Label)
+    self.styleAliceLable(label: self.alice3Label)
+    self.alice1Label.isHidden = false
+
+    self.styleUserLabel(label: self.user1Label)
+    self.styleUserLabel(label: self.user3Label)
+
+    self.waveView.isHidden = true
+    self.waveView.numberOfWaves = 4
+    self.waveView.waveColor = UIColor.dodgerBlue
+    self.waveView.backgroundColor = UIColor.clear
+
+    self.dividerLabel.text = ""
+    self.dividerLabel.backgroundColor = UIColor.dodgerBlue
+
+    self.recordingBacground.text = ""
+    self.recordingBacground.backgroundColor = UIColor.vividPurple
+
+    self.recordButton.setTitle("", for: .normal)
+    self.recordButton.layer.cornerRadius = 0.5 * self.recordButton.bounds.size.width
+    self.recordButton.clipsToBounds = true
+    self.recordButton.layer.borderWidth = 2
+    self.recordButton.layer.borderColor = UIColor.white.cgColor
+  }
+
+  @IBAction func applyButtonPushed(_ sender: Any) {
+    self.container2.isHidden = false
+  }
+
+  @IBAction func recordingButtonDown(_ sender: Any) {
+  }
+
+  @IBAction func recordingButtonTouchUp(_ sender: Any) {
   }
 
   fileprivate func styleKeyLabel(label: UILabel) {
@@ -67,5 +131,24 @@ class UserStoryView: UIViewController, WithViewModel {
   fileprivate func styleValueLabel(label: UILabel) {
     label.textColor = UIColor.dodgerBlueTwo
     label.font = UIFont.summaryValueFontFont()
+  }
+
+  fileprivate func styleAliceLable(label: UILabel) {
+    label.isHidden = true
+    label.numberOfLines = 0
+    label.lineBreakMode = .byWordWrapping
+    label.textAlignment = .left
+    label.textColor = UIColor.darkishBlue
+    label.font = UIFont.recordHelpMessageFont()
+  }
+
+  fileprivate func styleUserLabel(label: UILabel) {
+    label.text = ""
+    label.isHidden = true
+    label.numberOfLines = 0
+    label.lineBreakMode = .byWordWrapping
+    label.textAlignment = .right
+    label.textColor = UIColor.charcoalGrey
+    label.font = UIFont.userSpeechResponseFontFont()
   }
 }
