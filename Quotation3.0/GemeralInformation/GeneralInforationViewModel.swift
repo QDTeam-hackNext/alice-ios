fffc140e41f38ff937539544ff05d7f129cf523c
Dariusz Luksza <dariusz@luksza.org>
2017-04-22 14:09:35 +0200

    Refresh user data more often
    Userdata are obtained asynchronously therefore we should try to get them
    more often to limit the change of not having them and presenting partial
    messages to the user.

--- Quotation3.0/GemeralInformation/GeneralInforationViewModel.swift
+++ Quotation3.0/GemeralInformation/GeneralInforationViewModel.swift
@@ -21,9 +21,7 @@ class GeneralInformationViewModel: ViewModel {
   }
 
   func userRealName() -> String {
-    if self.userData == nil {
-      self.fetchUserData()
-    }
+    self.fetchUserData()
     return self.userData?.givenName ?? ""
   }
 
@@ -31,8 +29,10 @@ class GeneralInformationViewModel: ViewModel {
     let agreement = Agreement(period: period, smokes: smokes, startDate: "2017-04-30", sum: sum)
     let quoteData = QuoteData(birthDate: "\(self.userData?.birthday?.year ?? 1986)-\(self.userData?.birthday?.month ?? 5)-\(self.userData?.birthday?.day ?? 5)")
     let quote = Quote(agreement: agreement, data: quoteData)
+    self.fetchUserData()
     self.backend.quickQuote(quote) {
       result in
+      self.fetchUserData()
       if let r = result {
         callback("\(r)")
       }
@@ -40,6 +40,9 @@ class GeneralInformationViewModel: ViewModel {
   }
 
   fileprivate func fetchUserData() {
+    if self.userData != nil {
+      return
+    }
     self.userInfo.loadUserContact(callback: {
       userData in
       self.userData = userData
